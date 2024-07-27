ccffbd56bbfa45875a9ffc8ccb52b920406b0557
Amogh Dodawad <amoghasdodawad@gmail.com>
2024-07-28 02:16:12 +0530

    Scheduler almost completed

--- schedulers/queues.js
+++ schedulers/queues.js
@@ -5,15 +5,32 @@ class Node {
     }
 }
 
+/*
+    Follows a linked-list implementation of queue.
+ */
 class Queue {
-    #front = null;
-    #rear = null;
-    size = 0;
+    #front;
+    #rear;
+    size;
 
+    constructor(){
+        this.#front = null;
+        this.#rear = null;
+        this.size = 0;
+    }
+
+    /**
+     * Function to check is the queue is empty
+     * @returns {boolean} Returns true if the size is 0, false otherwise
+     */
     isEmpty(){
         return this.size === 0;
     }
 
+    /**
+     * Add callbacks to the queue.
+     * @param {function} callback - Accepts a callback function that needs to registered in the queue
+     */
     enQueue(data){
         const node = new Node(data);
         if(this.#rear === null){
@@ -25,6 +42,9 @@ class Queue {
         this.size++;
     }
 
+    /**
+     * Removes the function at the beginning of the queue.
+     */
     deQueue(){
         if(this.isEmpty()){
             return null;
@@ -32,7 +52,11 @@ class Queue {
         this.#front = this.#front.next;
         this.size--;
     }
-
+    
+    /**
+     * Returns the function at the beginning of the queue.
+     * @returns {function} - Function that needs to be executed next.
+     */
     front(){
         if(this.isEmpty()) return null;
         return this.#front.data;
--- schedulers/scheduler.js
+++ schedulers/scheduler.js
@@ -3,68 +3,36 @@ const Queue = require('./queues');
 class Scheduler {
 
     constructor(){
-        this.availableFileDescriptors = 40;
+        this.availableFileDescriptors = 20;
         this.waitQueue = new Queue();
     }
 
-    async schedule(callback) {
-        return new Promise(async ( resolve, reject ) => {
-            try {
-                if(this.availableFileDescriptors > 0) {
-                    await this.runFunction(callback);
-                    resolve();
-                } else {
-                    await new Promise( ( res, rej ) => {
-                        this.waitQueue.enQueue(() => this.runFunction(callback).then(res).catch(rej));
-                    });
-                    resolve();
-                }
-            } catch (err) {
-                reject(err)
-            }
-        });
-    }
+    /**
+        * This function accepts a callback that has to be scheduled.
+        * @param {function} callback - Callback function that has to be scheduled and executed.
+        * @returns {Promise<void>}  Returns a promise on settlement
+    */
+    async schedule(callback) {}
 
-    async acquireFileDescriptor(){
-        return new Promise(( resolve, reject ) => { 
-            if(this.availableFileDescriptors > 0) {
-                this.availableFileDescriptors--;
-                resolve();
-            } else {
-                reject({ message: 'File descriptors unavailable', success: false })
-            }
-        })
-    }
+    
+    /**
+        * @returns {Promise<void>} - Resolves on successfully acquiring the file descriptor, rejects otherwise.
+    */
+    async acquireFileDescriptor() {}
 
-    async releaseFileDescriptor(){
-        return new Promise(( resolve, reject ) => {
-            try {
-                this.availableFileDescriptors++;
-                if(this.availableFileDescriptors > 0 && this.waitQueue.size > 0){
-                    const nextFunction = this.waitQueue.front();
-                    this.waitQueue.deQueue();
-                    setImmediate(() => nextFunction());
-                }
-            resolve();
-            } catch {
-                reject({ message: 'Error releasing descriptor', success: false })
-            }
-        })
-    }
 
-    async runFunction(callback){
-        return new Promise(async ( resolve, reject ) => {
-            try {
-                await this.acquireFileDescriptor();
-                await callback();
-                await this.releaseFileDescriptor();
-                resolve();
-            } catch (err) {
-                reject();
-            }
-        })
+    /**
+        * @returns {Promise<void>} - Resolves on successfully releasing the file descriptor, rejects otherwise.
+    */
+    async releaseFileDescriptor() {}
 
-    }
+
+    /**
+        * @param {function} callback - Callback function that has to be scheduled and executed
+        * @returns {Promise<void>} - Returns a promise on settlement
+    */
+    async runFunction(callback) {}
+
 };
 
 Scheduler.prototype.schedule = async function (callback) {
@@ -74,6 +42,11 @@ Scheduler.prototype.schedule = async function (callback) {
                 await this.runFunction(callback);
                 resolve();
             } else {
+                /*
+                    - This is where we register the callback in the wait queue. 
+                    - Once the file descriptors are released, we will execute the callback function from the wait queue.
+                    - The logic for this is written in the runFunction method.
+                */
                 await new Promise(( res, rej ) => {
                     this.waitQueue.enQueue(
                         () => this.runFunction(callback).then(res).catch(rej)
@@ -83,6 +56,51 @@ Scheduler.prototype.schedule = async function (callback) {
             }
         } catch (err) {
             reject(err)
+        }
+    });
+}
+
+Scheduler.prototype.acquireFileDescriptor = async function() {
+    return new Promise(( resolve, reject ) => { 
+        if(this.availableFileDescriptors > 0) {
+            this.availableFileDescriptors--;
+            resolve();
+        } else {
+            reject({ message: 'File descriptors unavailable', success: false });
+        }
+    });
+}
+
+Scheduler.prototype.releaseFileDescriptor = async function () {
+    return new Promise(( resolve, reject ) => {
+        try {
+            this.availableFileDescriptors++;
+            if(this.availableFileDescriptors > 0 && this.waitQueue.size > 0){
+                const nextFunction = this.waitQueue.front();
+                this.waitQueue.deQueue();
+
+                /*
+                    - We make use of setImmediate so that the function can be executed ASAP.
+                    - Also the current function(releaseFileDescriptor) is popped from the stack if we use the above technique.
+                */
+                setImmediate(() => nextFunction());
+            }
+            resolve();
+        } catch {
+            reject({ message: 'Error releasing descriptor', success: false });
+        }
+    });
+}
+
+Scheduler.prototype.runFunction = async function(callback) {
+    return new Promise(async ( resolve, reject ) => {
+        try {
+            await this.acquireFileDescriptor();
+            await callback();
+            await this.releaseFileDescriptor();
+            resolve();
+        } catch (err) {
+            reject();
         }
     });
 }
